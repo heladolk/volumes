6e6ef5695dafbf1df5bec3354c93dd769a7527a0
heladolk <heladolk@yahoo.com>
2016-04-19 15:36:50 -0700

    include solution for volume exercise

---
+++ volumes.js
@@ -0,0 +1,27 @@
+// Sphere or Cone
+
+var pi = Math.PI;
+
+var sphereRadius = 15;
+
+var coneBase = 11;
+var coneHeight = 22;
+
+var rectLength = 5;
+var rectHeight = 11;
+var rectWidth = 467;
+
+// the volume of a sphere
+var sphereVolume = (4 / 3) * pi * Math.pow(sphereRadius, 3);
+//console.log(sphereVolume);
+
+// the volume of a cone
+var coneVolume = (1 / 3) * coneBase * coneHeight;
+//console.log(coneVolume);
+
+// volume of a rectangular prism
+var rectPrismVolume = rectLength * rectHeight * rectWidth;
+//console.log(rectPrismVolume);
+
+
+
